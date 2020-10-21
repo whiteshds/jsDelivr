e0d9ca26f7a031b5abfd7e4c68bf00c8b5aa974e
whiteshds <2689658133@qq.com>
2020-10-21 09:02:44 +0800

    666

---
+++ txt/仓库/图廊/js/gallerysuijitupian.js
@@ -0,0 +1,8 @@
+function gallerysuijitupian(){var t=new Array;
+    if(t[0]="https://whiteshds.ml/Gallery/01/",
+    t[1]="https://whiteshds.ml/Gallery/02/",
+    t[2]="https://whiteshds.ml/Gallery/03/",
+    t[3]="https://whiteshds.ml/Gallery/04/",
+        document.referrer){var s=new URL(document.referrer).origin;
+        t.splice(t.indexOf(s),1)}var h=Math.floor(Math.random()*t.length);
+    window.location=t[h]}
